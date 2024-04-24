1d56e2700709f56895df09824d148fe670de7a7d
unknown <techcomk@gmail.com>
2024-04-24 20:58:03 -0300

    Atualizando o projeto

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-st.header("Hello world")
-
-st.write()
-
 df = pd.read_csv("https://raw.githubusercontent.com/hermeson883/data_science_workshop/main/linguagens.csv")
 
 st.dataframe(df)
+
+linguagens = df["Linguagem"].unique()
+
+escolha = st.selectbox("Escolha um", linguagens)
+
+if st.button("Click em mim"):
+    filtro = df.loc[df["Linguagem"] == escolha]
+    st.write(filtro)
