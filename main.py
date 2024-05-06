31ec66435c59cc7ece65beca17d6d0f15ce3d6ef
unknown <techcomk@gmail.com>
2024-05-06 20:26:59 -0300

    inserindo graficos

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
-
+import plotly.express as px
 df = pd.read_csv("https://raw.githubusercontent.com/hermeson883/data_science_workshop"
                  "/main/linguagens.csv")
 
-st.bar_chart(df, x='Linguagem', y='Desenvolvedores')
+grafico = px.pie(df, names='Linguagem')
+grafico2 = px.bar(df, color='Linguagem', x='Linguagem',
+                  y='Desenvolvedores')
+st.plotly_chart(grafico)
+st.plotly_chart(grafico2)
