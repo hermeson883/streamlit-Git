1f64a0f4282dc5566d6bba48245cd7451449be2e
unknown <techcomk@gmail.com>
2024-05-06 19:28:22 -0300

    testando o restore

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("https://raw.githubusercontent.com/hermeson883/data_science_workshop/main/linguagens.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/hermeson883/data_science_workshop"
+                 "/main/linguagens.csv")
 
-st.dataframe(df)
-
-linguagens = df["Linguagem"].unique()
-
-escolha = st.selectbox("Escolha um", linguagens)
-
-if st.button("Click em mim"):
-    filtro = df.loc[df["Linguagem"] == escolha]
-    st.write(filtro)
+st.bar_chart(df, x='Linguagem', y='Desenvolvedores')
